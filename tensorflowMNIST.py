e5992257779b29860c832591ef7311b389e6db26
Pardo7 <walterpardo7@gmail.com>
2017-08-27 12:49:52 -0700

    Gathering and structuring notMNIST dataset

---
+++ tensorflowMNIST.py
@@ -0,0 +1,87 @@
+import hashlib
+import os
+import pickle
+from urllib.request import urlretrieve
+
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils import resample
+from tqdm import tqdm
+from zipfile import ZipFile
+
+print('All modules imported.')
+
+# Fileset is too large for most computers to handle. Utilizing a subset of the data
+# 15,000 images for each label (A-J)
+def download(url, file):
+    """
+    Download file from <url>
+    :param url: URL to file
+    :param file: Local file path
+    """
+    if not os.path.isfile(file):
+        print('Downloading ' + file + '...')
+        urlretrieve(url, file)
+        print('Download Finished')
+
+# Downloading the training and test dataset.
+download('https://s3.amazonaws.com/udacity-sdc/notMNIST_train.zip', 'notMNIST_train.zip')
+download('https://s3.amazonaws.com/udacity-sdc/notMNIST_test.zip', 'notMNIST_test.zip')
+
+# Ensuring files aren't corrupted
+assert hashlib.md5(open('notMNIST_train.zip', 'rb').read()).hexdigest() == 'c8673b3f28f489e9cdf3a3d74e2ac8fa',\
+        'notMNIST_train.zip file is corrupted.  Remove the file and try again.'
+assert hashlib.md5(open('notMNIST_test.zip', 'rb').read()).hexdigest() == '5d3c7e653e63471c88df796156a9dfa9',\
+        'notMNIST_test.zip file is corrupted.  Remove the file and try again.'
+
+# Waiting until all files have been downloaded.
+print('All files downloaded.')
+
+
+def uncompress_features_labels(file):
+    """
+    Uncompressing features and labels from a zip file
+    :param file: The zip file to extract the data from
+    """
+    features = []
+    labels = []
+
+    with ZipFile(file) as zipf:
+        # Progress Bar
+        filenames_pbar = tqdm(zipf.namelist(), unit='files')
+
+        # Getting features and labels from all files
+        for filename in filenames_pbar:
+            # Checking if the file is a directory
+            if not filename.endswith('/'):
+                with zipf.open(filename) as image_file:
+                    image = Image.open(image_file)
+                    image.load()
+                    # Loading image data as 1 dimensional array
+                    # Using float32 to save on memory space
+                    feature = np.array(image, dtype=np.float32).flatten()
+
+                # Getting the the letter from the filename.  This is the letter of the image.
+                label = os.path.split(filename)[1][0]
+
+                features.append(feature)
+                labels.append(label)
+    return np.array(features), np.array(labels)
+
+# Getting the features and labels from the zip files
+train_features, train_labels = uncompress_features_labels('notMNIST_train.zip')
+test_features, test_labels = uncompress_features_labels('notMNIST_test.zip')
+
+# Limiting the amount of data to work with a docker container
+docker_size_limit = 150000
+train_features, train_labels = resample(train_features, train_labels, n_samples=docker_size_limit)
+
+# Setting flags for feature engineering.  This will prevent from skipping an important step.
+is_features_normal = False
+is_labels_encod = False
+
+# Waiting until all features and labels have been uncompressed.
+print('All features and labels uncompressed.')
+
