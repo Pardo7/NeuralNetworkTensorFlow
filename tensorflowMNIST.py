55493697009eed5ed2c45993bb68d13dd3c4f350
Pardo7 <walterpardo7@gmail.com>
2017-08-27 13:06:44 -0700

    Implemented min max scaling along with test.

--- tensorflowMNIST.py
+++ tensorflowMNIST.py
@@ -85,3 +85,34 @@ is_labels_encod = False
 # Waiting until all features and labels have been uncompressed.
 print('All features and labels uncompressed.')
 
+# Implementing Min-Max scaling for grayscale image data
+def normalize_grayscale(image_data):
+    """
+    Normalizing image data with Min-Max scaling to a range of [0.1, 0.9]
+    :param image_data: The image data to be normalized
+    :return: Normalized image data
+    """
+    # Min-Max scaling for grayscale image data
+    a = 0.1
+    b = 0.9
+    greyscale_min = 0
+    greyscale_max = 255
+    return a + ( ( (image_data - greyscale_min) * (b - a) )/(greyscale_max - greyscale_min) )
+
+# Test Cases
+np.testing.assert_array_almost_equal(
+    normalize_grayscale(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 255])),
+    [0.1, 0.103137254902, 0.106274509804, 0.109411764706, 0.112549019608, 0.11568627451, 0.118823529412, 0.121960784314,
+     0.125098039216, 0.128235294118, 0.13137254902, 0.9],
+    decimal=3)
+np.testing.assert_array_almost_equal(
+    normalize_grayscale(np.array([0, 1, 10, 20, 30, 40, 233, 244, 254,255])),
+    [0.1, 0.103137254902, 0.13137254902, 0.162745098039, 0.194117647059, 0.225490196078, 0.830980392157, 0.865490196078,
+     0.896862745098, 0.9])
+
+if not is_features_normal:
+    train_features = normalize_grayscale(train_features)
+    test_features = normalize_grayscale(test_features)
+    is_features_normal = True
+
+print('Tests Passed!')
