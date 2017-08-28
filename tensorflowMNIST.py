4621511715467ae8dde79ae2a3503a63f65bdf34
Pardo7 <walterpardo7@gmail.com>
2017-08-27 19:33:16 -0700

    Implemented environment.yml, one hot encoding, linear function, cross entropy, parameter config, gradient descent, additional test cases.

--- tensorflowMNIST.py
+++ tensorflowMNIST.py
@@ -116,3 +116,254 @@ if not is_features_normal:
     is_features_normal = True
 
 print('Tests Passed!')
+
+if not is_labels_encod:
+    # Turning labels into numbers and applying One-Hot Encoding
+    encoder = LabelBinarizer()
+    encoder.fit(train_labels)
+    train_labels = encoder.transform(train_labels)
+    test_labels = encoder.transform(test_labels)
+
+    # Changing to float32, so it can be multiplied against the features in TensorFlow, which are float32
+    train_labels = train_labels.astype(np.float32)
+    test_labels = test_labels.astype(np.float32)
+    is_labels_encod = True
+
+print('Labels One-Hot Encoded')
+
+assert is_features_normal, 'Skipped step to normalize the features'
+assert is_labels_encod, 'Skipped step to One-Hot Encode the labels'
+
+# Getting randomized datasets for training and validation
+train_features, valid_features, train_labels, valid_labels = train_test_split(
+    train_features,
+    train_labels,
+    test_size=0.05,
+    random_state=832289)
+
+print('Training features and labels randomized and split.')
+
+# Saving the data for easy access
+pickle_file = 'notMNIST.pickle'
+if not os.path.isfile(pickle_file):
+    print('Saving data to pickle file...')
+    try:
+        with open('notMNIST.pickle', 'wb') as pfile:
+            pickle.dump(
+                {
+                    'train_dataset': train_features,
+                    'train_labels': train_labels,
+                    'valid_dataset': valid_features,
+                    'valid_labels': valid_labels,
+                    'test_dataset': test_features,
+                    'test_labels': test_labels,
+                },
+                pfile, pickle.HIGHEST_PROTOCOL)
+    except Exception as e:
+        print('Unable to save data to', pickle_file, ':', e)
+        raise
+
+print('Data cached in pickle file.')
+
+
+# matplotlib inline
+
+# Loading modules
+import pickle
+import math
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# Reloading data
+pickle_file = 'notMNIST.pickle'
+with open(pickle_file, 'rb') as f:
+  pickle_data = pickle.load(f)
+  train_features = pickle_data['train_dataset']
+  train_labels = pickle_data['train_labels']
+  valid_features = pickle_data['valid_dataset']
+  valid_labels = pickle_data['valid_labels']
+  test_features = pickle_data['test_dataset']
+  test_labels = pickle_data['test_labels']
+  del pickle_data  # Free up memory
+
+print('Data and modules loaded.')
+
+
+# All the pixels in the image (28 * 28 = 784)
+features_count = 784
+# All the labels
+labels_count = 10
+
+# Setting the features and labels tensors
+features = tf.placeholder(tf.float32)
+labels = tf.placeholder(tf.float32)
+
+# SetTing the weights and bias tensors
+weights = tf.Variable(tf.truncated_normal((features_count, labels_count)))
+biases = tf.Variable(tf.zeros(labels_count))
+
+# Test Cases
+from tensorflow.python.ops.variables import Variable
+
+assert features._op.name.startswith('Placeholder'), 'features must be a placeholder'
+assert labels._op.name.startswith('Placeholder'), 'labels must be a placeholder'
+assert isinstance(weights, Variable), 'weights must be a TensorFlow variable'
+assert isinstance(biases, Variable), 'biases must be a TensorFlow variable'
+
+assert features._shape == None or (\
+    features._shape.dims[0].value is None and\
+    features._shape.dims[1].value in [None, 784]), 'The shape of features is incorrect'
+assert labels._shape  == None or (\
+    labels._shape.dims[0].value is None and\
+    labels._shape.dims[1].value in [None, 10]), 'The shape of labels is incorrect'
+assert weights._variable._shape == (784, 10), 'The shape of weights is incorrect'
+assert biases._variable._shape == (10), 'The shape of biases is incorrect'
+
+assert features._dtype == tf.float32, 'features must be type float32'
+assert labels._dtype == tf.float32, 'labels must be type float32'
+
+# Feed dicts for training, validation, and test session
+train_feed_dict = {features: train_features, labels: train_labels}
+valid_feed_dict = {features: valid_features, labels: valid_labels}
+test_feed_dict = {features: test_features, labels: test_labels}
+
+# Linear Function WX + b
+logits = tf.matmul(features, weights) + biases
+
+prediction = tf.nn.softmax(logits)
+
+# Cross entropy
+cross_entropy = -tf.reduce_sum(labels * tf.log(prediction), reduction_indices=1)
+
+# Training loss
+loss = tf.reduce_mean(cross_entropy)
+
+# Creating an operation that initializes all variables
+init = tf.global_variables_initializer()
+
+# Test Cases
+with tf.Session() as session:
+    session.run(init)
+    session.run(loss, feed_dict=train_feed_dict)
+    session.run(loss, feed_dict=valid_feed_dict)
+    session.run(loss, feed_dict=test_feed_dict)
+    biases_data = session.run(biases)
+
+assert not np.count_nonzero(biases_data), 'biases must be zeros'
+
+print('Tests Passed!')
+
+
+# Determining if the predictions are correct
+is_correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
+# Calculating the accuracy of the predictions
+accuracy = tf.reduce_mean(tf.cast(is_correct_prediction, tf.float32))
+
+print('Accuracy function created.')
+
+
+# Change if there are memory restrictions
+batch_size = 128
+
+# parameters for each configuration
+epochs = 20
+learning_rate = 0.1
+
+# Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+# The accuracy measured against the validation set
+validation_accuracy = 0.0
+
+# Measurements use for graphing loss and accuracy
+log_batch_step = 50
+batches = []
+loss_batch = []
+train_acc_batch = []
+valid_acc_batch = []
+
+with tf.Session() as session:
+    session.run(init)
+    batch_count = int(math.ceil(len(train_features)/batch_size))
+
+    for epoch_i in range(epochs):
+
+        # Progress bar
+        batches_pbar = tqdm(range(batch_count), desc='Epoch {:>2}/{}'.format(epoch_i+1, epochs), unit='batches')
+
+        #  Training cycle
+        for batch_i in batches_pbar:
+            # Getting a batch of training features and labels
+            batch_start = batch_i*batch_size
+            batch_features = train_features[batch_start:batch_start + batch_size]
+            batch_labels = train_labels[batch_start:batch_start + batch_size]
+
+            # Running optimizer and getting the loss
+            _, l = session.run(
+                [optimizer, loss],
+                feed_dict={features: batch_features, labels: batch_labels})
+
+            # Logging every 50 batches
+            if not batch_i % log_batch_step:
+                # Calculating Training and Validation accuracy
+                training_accuracy = session.run(accuracy, feed_dict=train_feed_dict)
+                validation_accuracy = session.run(accuracy, feed_dict=valid_feed_dict)
+
+                # Logging batches
+                previous_batch = batches[-1] if batches else 0
+                batches.append(log_batch_step + previous_batch)
+                loss_batch.append(l)
+                train_acc_batch.append(training_accuracy)
+                valid_acc_batch.append(validation_accuracy)
+
+        # Checking accuracy against Validation data
+        validation_accuracy = session.run(accuracy, feed_dict=valid_feed_dict)
+
+loss_plot = plt.subplot(211)
+loss_plot.set_title('Loss')
+loss_plot.plot(batches, loss_batch, 'g')
+loss_plot.set_xlim([batches[0], batches[-1]])
+acc_plot = plt.subplot(212)
+acc_plot.set_title('Accuracy')
+acc_plot.plot(batches, train_acc_batch, 'r', label='Training Accuracy')
+acc_plot.plot(batches, valid_acc_batch, 'x', label='Validation Accuracy')
+acc_plot.set_ylim([0, 1.0])
+acc_plot.set_xlim([batches[0], batches[-1]])
+acc_plot.legend(loc=4)
+plt.tight_layout()
+plt.show()
+
+print('Validation accuracy at {}'.format(validation_accuracy))
+
+# Accuracy measured against the test set
+test_accuracy = 0.0
+
+with tf.Session() as session:
+
+    session.run(init)
+    batch_count = int(math.ceil(len(train_features)/batch_size))
+
+    for epoch_i in range(epochs):
+
+        # Progress bar
+        batches_pbar = tqdm(range(batch_count), desc='Epoch {:>2}/{}'.format(epoch_i+1, epochs), unit='batches')
+
+        # The training cycle
+        for batch_i in batches_pbar:
+            # Getting a batch of training features and labels
+            batch_start = batch_i*batch_size
+            batch_features = train_features[batch_start:batch_start + batch_size]
+            batch_labels = train_labels[batch_start:batch_start + batch_size]
+
+            # Running optimizer
+            _ = session.run(optimizer, feed_dict={features: batch_features, labels: batch_labels})
+
+        # Checking accuracy against Test data
+        test_accuracy = session.run(accuracy, feed_dict=test_feed_dict)
+
+
+assert test_accuracy >= 0.80, 'Test accuracy at {}, should be equal to or greater than 0.80'.format(test_accuracy)
+print('Perfect, The Test Accuracy is {}'.format(test_accuracy))
